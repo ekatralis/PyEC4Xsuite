44c64c7e09ffd96773f67c88214e811ebf0794a8
Evangelos Katralis <vkatralis@gmail.com>
2025-09-15 16:45:21 +0200

    Fix bugs with empty slices and NaNs

--- PyEC4XS.py
+++ PyEC4XS.py
@@ -34,6 +34,8 @@ class XsuiteUniformBinSlicer:
         self.particles = particles
         self._set_active_particle_idx()
         self._set_bins()
+        self.beta_avg = np.mean(self.particles.beta0[self.active_particles])
+        self.gamma_avg = np.mean(self.particles.gamma0[self.active_particles])
     
     def _set_active_particle_idx(self):
         self.active_particles = np.where(self.particles.state>0)[0]
@@ -72,16 +74,20 @@ class XsuiteUniformBinSlicer:
 
     def _get_slice(self, slice_num: int) -> dict:
         idx_particle_slice = self._particles_in_slice(slice_num)
-        beta = np.mean(self.particles.beta0[idx_particle_slice])
-        gamma = 1/(np.sqrt(1-beta**2))
-        dt = self.dz / (beta * c)
-        z_mean = np.mean(self.particles.zeta[idx_particle_slice])
         active_particles = len(idx_particle_slice)
+        if active_particles > 0:
+            beta = np.mean(self.particles.beta0[idx_particle_slice])
+            gamma = 1/(np.sqrt(1-beta**2))
+        else:
+            beta = self.beta_avg
+            gamma = self.gamma_avg
+        dt = self.dz / (beta * c)
+        z_center = (self.bins[slice_num+1] + self.bins[slice_num]) / 2
         slice_dict = {
             "num_active"   : active_particles,
             "particle_idx" : idx_particle_slice,
             "beta"         : beta,
-            "zeta"         : z_mean,
+            "zeta"         : z_center,
             "gamma"        : gamma,
             "dz"           : self.dz,
             "dt"           : dt, 
@@ -257,13 +263,16 @@ class xEcloud:
     def _track_single_slice(self, particles: xt.Particles, slice: dict, force_pyecl_newpass: bool = False):
         spacech_ele = self.cloudsim.spacech_ele
         # Check if the slice interacts with the beam
-        if "slice_info" in list(slice.keys()):
-            if "interact_with_EC" in list(slice["slice_info"].keys()):
-                interact_with_EC = slice["slice_info"]["interact_with_EC"]
+        if slice["num_active"] > 0:
+            if "slice_info" in list(slice.keys()):
+                if "interact_with_EC" in list(slice["slice_info"].keys()):
+                    interact_with_EC = slice["slice_info"]["interact_with_EC"]
+                else:
+                    interact_with_EC = True
             else:
                 interact_with_EC = True
         else:
-            interact_with_EC = True
+            interact_with_EC = False
         
         dt_slice = slice["dt"]
         ix = slice["particle_idx"]
